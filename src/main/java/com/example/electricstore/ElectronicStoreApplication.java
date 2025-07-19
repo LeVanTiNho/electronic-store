4a2175a6f33a57593ba999dca8bfccf22b6b804d
Lê Văn Tí Nho <levantinho@MacBook-Air-cua-Nhi.local>
2025-07-20 01:22:38 +0700

    change project name, package name

--- src/main/java/com/example/electricstore/ElectronicStoreApplication.java
+++ src/main/java/com/example/electricstore/ElectronicStoreApplication.java
@@ -1,4 +1,4 @@
-package com.example.my_spring_project;
+package com.example.electricstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
